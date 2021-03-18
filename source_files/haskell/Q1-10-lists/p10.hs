be39c6d444655e1193e25854f376ae36c703f0bb
Ben Fidlers <ben.fidlers@student.kuleuven.be>
2021-03-19 00:39:44 +0100

    add solution p10

---
+++ source_files/haskell/Q1-10-lists/p10.hs
@@ -0,0 +1,8 @@
+-- Run-length encoding of a list.
+-- Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+-- Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+
+import Data.List 
+
+encode :: Eq a => [a] -> [(Int, a)]
+encode = map (\ a -> (length a, head a)) . group
