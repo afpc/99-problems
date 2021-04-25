2990ecb95c0cd54543fe8e8bc8fce662596d2a89
Ben Fidlers <ben.fidlers@gmail.com>
2021-04-25 12:34:28 +0200

    fix p16

--- ben/haskell/Q11-20-lists-cont/p16.hs
+++ ben/haskell/Q11-20-lists-cont/p16.hs
@@ -1,8 +1,11 @@
 -- With Prelude
 dropEvery :: [a] -> Int -> [a]
-dropEvery list i = take (i - 1) list ++ drop i list
+dropEvery [] _ = []
+dropEvery list i = take (i - 1) list ++ dropEvery (drop i list) i
 
 -- With list recursion
 dropEvery' :: [a] -> Int -> [a]
-dropEvery' (x:xs) 1 = xs 
-dropEvery' (x:xs) i = x : dropEvery' xs (i-1)
+dropEvery' list i = dropEveryHelp list i i 
+    where dropEveryHelp [] _ _ = []
+          dropEveryHelp (x:xs) nb 1 = dropEveryHelp xs nb nb 
+          dropEveryHelp (x:xs) nb i = x : dropEveryHelp xs nb (i-1)
