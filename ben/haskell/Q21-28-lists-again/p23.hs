86c006b09161a04bed05e834702aee45c566be80
Ben Fidlers <ben.fidlers@gmail.com>
2021-05-06 16:04:52 +0200

    improvement p23

--- ben/haskell/Q21-28-lists-again/p23.hs
+++ ben/haskell/Q21-28-lists-again/p23.hs
@@ -6,3 +6,8 @@ rnd_select :: [a] -> Int -> IO [a]
 rnd_select list i = do g <- getStdGen
                        let nbs = take i (randomRs (0, length list - 1) g)
                            in return (foldr (\a b -> (list!!a):b) [] nbs) 
+
+-- Somewhat nicer
+rnd_select' :: [a] -> Int -> IO [a]
+rnd_select' list i = do g <- getStdGen
+                        return $ take i [list!!n | n <- (randomRs (0, length list - 1) g)]
