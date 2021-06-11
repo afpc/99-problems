86f2e9f555cffc3914a880017b0bb10ae9f5e75b
Ben Fidlers <ben.fidlers@gmail.com>
2021-06-11 12:35:44 +0200

    solution p13 prolog

---
+++ ben/prolog/P01-P28-lists/p13.pl
@@ -0,0 +1,18 @@
+% Modified run-length encoding.
+
+count([], _, [], 0). 
+count([X|Xs], X, R, N1) :-
+    count(Xs, X, R, N0),
+    N1 is N0 + 1.
+count([X|Xs], Y, [X|Xs], 0) :- 
+    X \= Y.
+
+encode_direct([], []).
+encode_direct([Y|Ys], [Y |Xs]) :-
+    count([Y|Ys], Y, L, X),
+    X == 1,
+    encode_direct(L, Xs).
+encode_direct([Y|Ys], [[X, Y] |Xs]) :-
+    count([Y|Ys], Y, L, X),
+    X \= 1,
+    encode_direct(L, Xs).
