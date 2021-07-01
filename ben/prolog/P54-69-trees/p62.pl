d6d331c7377542355819a11cb4ed41eb7816a4c3
Ben Fidlers <ben.fidlers@gmail.com>
2021-07-01 22:42:46 +0200

    renamed files

--- ben/prolog/P54-69-trees/p62.pl
+++ ben/prolog/P54-69-trees/p62.pl
@@ -1,9 +1,9 @@
-% Collect the leaves of a binary tree in a list
-% A leaf is a node with no successors. Write a predicate leaves/2 to collect them in a list.
+% Collect the internal nodes of a binary tree in a list
+% An internal node of a binary tree has either one or two non-empty successors. Write a predicate internals/2 to collect them in a list.
 
-leaves(nil, []).
-leaves(t(X, nil, nil), [X]) :- !.
-leaves(t(_, L, R), N) :-
-    leaves(L, NL), 
-    leaves(R, NR),
+internals(nil, []).
+internals(t(_, nil, nil), []) :- !.
+internals(t(X, L, R), [X|N]) :-
+    internals(L, NL), 
+    internals(R, NR),
     append(NL, NR, N).
