8af5ae8168ac24591f8559354d6077b11d348d1d
Ben Fidlers <ben.fidlers@gmail.com>
2021-07-07 23:54:02 +0200

    solution p36 haskell

---
+++ ben/haskell/Q31-41-arithmetic/p36.hs
@@ -0,0 +1,18 @@
+-- Determine the prime factors of a given positive integer. Construct a flat list containing the prime factors in ascending order.
+
+import Data.List
+
+prime_factors_mult :: Int -> [(Int, Int)]
+prime_factors_mult = map (\ x -> (head x, length x)) . group . primeFactors
+
+primeFactors :: Int -> [Int]
+primeFactors n0
+    | n0 < 2 = []
+    | otherwise = p : (primeFactors n1)
+        where p = nextPrime 2 n0
+              n1 = div n0 p
+
+nextPrime :: Int -> Int -> Int
+nextPrime min n 
+    | mod n min == 0 = min
+    | otherwise = nextPrime (min + 1) n
