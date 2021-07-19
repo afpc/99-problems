d4bbfd9febf2ab39b45c3177c988a8c7fa81bbe7
Ben Fidlers <ben.fidlers@gmail.com>
2021-07-19 22:23:25 +0200

    solution p61 haskell

---
+++ ben/haskell/Q61-69-binary-trees-cont/p61.hs
@@ -0,0 +1,16 @@
+{-
+Count the leaves of a binary tree
+
+A leaf is a node with no successors. Write a predicate count_leaves/2 to count them.
+-}
+
+data Tree a = Empty | Branch a (Tree a) (Tree a)
+              deriving (Show, Eq)
+
+tree4 = Branch 1 (Branch 2 Empty (Branch 4 Empty Empty))
+                 (Branch 2 Empty Empty)
+
+countLeaves :: Tree a -> Int
+countLeaves Empty = 0
+countLeaves (Branch _ Empty Empty) = 1
+countLeaves (Branch _ l r) = countLeaves l + countLeaves r
