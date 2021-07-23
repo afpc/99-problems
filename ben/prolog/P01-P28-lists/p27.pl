d3fdff58843881607c1072e92c6e14be72130cf3
Ben Fidlers <ben.fidlers@gmail.com>
2021-07-23 23:24:33 +0200

    solution p27 prolog

--- ben/prolog/P01-P28-lists/p27.pl
+++ ben/prolog/P01-P28-lists/p27.pl
@@ -1,12 +1,29 @@
 % Group the elements of a set into disjoint subsets.
 
 group(_, [], []).
-group(L1, [X|Xs], [R|Rs]) :-  
-    subset(R, L1),
-    length(R, X), 
-    subtract(L1, R, L2), 
-    group(L2, Xs, Rs).
+group(L, [X|Xs], [S|Ss]) :-
+    selectN(X, L, S, _),
+    sub_list(S, L, R), 
+    group(R, Xs, Ss). 
 
-% selectN(0, S, S).
-% selectN(N, S, R) :-
-%    N > 0,     
+selectN(0, L, [], L).
+selectN(N, L, [E|S], R) :-
+    N > 0,
+    N1 is N - 1,
+    length(L, Length),
+    numlist(0, Length, NL),
+    member(I, NL),
+    nth1(I, L, E),
+    drop(I, L, L1), 
+    selectN(N1, L1, S, R).
+
+drop(0, L, L).
+drop(N, [_|Xs], R) :-
+    N > 0, 
+    N1 is N - 1,
+    drop(N1, Xs, R).
+
+sub_list([], L, L).
+sub_list([X|Xs], L, R) :-
+    select(X, L, L1), 
+    sub_list(Xs, L1, R).
